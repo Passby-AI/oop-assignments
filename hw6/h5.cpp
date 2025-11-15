678ad06ab1037a04992efd0fd392ce9c351c3f18
Passby-AI <69393270+Passby-AI@users.noreply.github.com>
2025-11-15 22:32:13 +0800

    Add comments to reverse function for clarity
    Added comments to clarify the logic of the reverse function.

--- hw6/h5.cpp
+++ hw6/h5.cpp
@@ -11,10 +11,14 @@ void reverse(string &s)
 {
     if(s.length()<=1)
     {
+        //当长度小于等于1时，正序倒序一样，直接返回
         return;
     }
+    //保存第一个字符
     char first=s[0];
+    //保存除第一个字符外剩下的字符
     string rest=s.substr(1);
+    //递归，将剩下的字符串也倒置
     reverse(rest);
     s=rest+first;
 }
