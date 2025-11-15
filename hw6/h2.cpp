6283782bde6ddef440cf0e5f26561056586995b2
Passby-AI <69393270+Passby-AI@users.noreply.github.com>
2025-11-15 22:38:15 +0800

    Add comments to clarify pointer and reference usage

--- hw6/h2.cpp
+++ hw6/h2.cpp
@@ -9,12 +9,17 @@ using namespace std;
 
 int main()
 {
+    //将a先初始化为1
     int a=1;
+    //输出a的值
     cout<<"a: "<<a<<endl;
+    //声明指针p和引用 r
     int *p=&a;
     int &r =a;
+    //通过指针把值修改为10，输出此时a的值
     *p=10;
     cout<<"a: "<<a<<" *p: "<<*p<<endl;
+    //通过引用修改为5，输出a值
     r=5;
     cout<<"a: "<<a<<" r: "<<r<<endl;
     return 0;
