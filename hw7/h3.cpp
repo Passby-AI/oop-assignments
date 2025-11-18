b837927bd5f90462bf4da62eb22cbc904bb37fa3
Passby-AI <69393270+Passby-AI@users.noreply.github.com>
2025-11-18 15:27:18 +0800

    更新 h3.cpp
    Update with commments

--- hw7/h3.cpp
+++ hw7/h3.cpp
@@ -33,17 +33,18 @@ int main()
 {
 
     DerivedClass D{};
+//声明指向BaseClass类型的指针p1,指向DerivedClass类型的指针p2,都指向DerivedClass的实例D
     BaseClass* p1 = &D;
     DerivedClass* p2 = &D;
-
+//直接调用成员函数
     cout << "--- Object Call ---" << endl;
     D.fn1();
     D.fn2();
-
+//通过指向基类指针调用函数
     cout << "--- Base Pointer Call ---" << endl;
     p1->fn1();
     p1->fn2(); 
-
+//通过指向子类DerivedClass类型指针调用函数
     cout << "--- Derived Pointer Call ---" << endl;
     p2->fn1();
     p2->fn2(); 
